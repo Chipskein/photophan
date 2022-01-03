845d5b6f9ccab7f8cc359cbbe38b148a067c6520
Chipskein <abfn0905@gmail.com>
2022-01-03 09:30:01 -0300

    fix:Resolve bugzin dos comments

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,8 +19,8 @@ function Navbar({searchTerm,setSearch,user}) {
                 />
             </div>
             <div className="flex gap-3">
-                <Link to={`user-profile/${user?._id}`} className="hidden md:block">
-                    <img src={user.userimage} alt="" className="w-14 h-12 rounded-lg" />
+                <Link to={`user-profile/${user?.user?._id}`} className="hidden md:block">
+                    <img src={user?.user?.userimage} alt="" className="w-14 h-12 rounded-lg" />
                 </Link>
                 <Link to="create-pin" className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center">
                     <IoMdAdd />
